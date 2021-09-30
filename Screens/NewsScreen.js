c643d3170be21f36ce418b7bd62f375d241e7999
abhishek <abmishra700@gmail.com>
2021-09-30 14:50:26 +0530

    news carousel with react-native-snap-crousel

--- Screens/NewsScreen.js
+++ Screens/NewsScreen.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useContext, useState } from "react";
+import { View, Dimensions, StyleSheet } from "react-native";
+import Carousel from "react-native-snap-carousel";
+import { NewsContext } from "../API/Context";
+import SingleNews from "../components/SingleNews";
 
 const NewsScreen = () => {
-    return (
-        <View>
-            <Text>NewsScreen</Text>
-        </View>
-    )
-}
+  const {
+    news: { articles },
+  } = useContext(NewsContext);
 
-export default NewsScreen
+  const [activeIndex, setActiveIndex] = useState();
+
+  const windowHeight = Dimensions.get("window").height;
+
+  return (
+    <View style={styles.carousel}>
+      {articles && (
+        <Carousel
+          layout={"stack"}
+          data={articles.slice(0, 10)}
+          sliderHeight={300}
+          itemHeight={windowHeight}
+          vertical={true}
+          renderItem={({ item, index }) => (
+            <SingleNews item={item} index={index} />
+          )}
+          onSnapToItem={(index) => setActiveIndex(index)}
+        />
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  carousel: {
+    flex: 1,
+    transform: [{ scaleY: -1 }],
+    backgroundColor: "black",
+  },
+});
+
+export default NewsScreen;
